3df3f48d03050e927cf6a458ab33c54c3b378c9d
Aaron Rockele <aaron_rockele@MacBook-Pro-van-Aaron.local>
2019-06-19 10:27:08 +0200

    made a canvas

---
+++ Assets/JS/script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+const rows = canvan.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
